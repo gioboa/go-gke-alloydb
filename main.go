0affe918086b1641493974038c5a6fdd17220950
gioboa <giorgiob.boa@gmail.com>
2026-03-23 13:56:03 +0100

    chore: add Gin webserver 🍸

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	_ = r.Run(":8080")
+}
